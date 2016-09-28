832b62393c2add1de88361529ce1c87c03e25ffc
Paul Scherer <paul.scherer@mrc-epid.cam.ac.uk>
2016-09-28 14:19:26 +0100

    up to date on the data simulation r

--- data_simulation_practice.R
+++ data_simulation_practice.R
@@ -22,29 +22,87 @@ set_beta <- 0.5
 
 # Index properties (Assumption that they are Gaussian)
 # Format : (mean, stdev)
-index1 <- c(10,5)
-index2 <- c(20,5)
-index3 <- c(30,5)
-index4 <- c(40,5)
+index_mean1 <- 35.6
+index_stdev1 <- 13.7
+index_mean2 <- 43.7
+index_stdev2 <- 15.2
+index_mean3 <- 49.0
+index_stdev3 <- 17.9
+index_mean4 <- 56.2
+index_stdev4 <- 18.4
+
+# PAEE range
+paee_range_min <- 20
+paee_range_max <- 75
 
 ###############################################################################
 ############################# Functions #######################################
 ###############################################################################
-data_generator <- function(exposure, set_beta){
-	# returns a datapoint given an exposure with some gaussian noise
+data_generator <- function(exposure, set_beta, constant, noise){
+	# returns a datapoint given an exposure
+	# relies on a set constant, gaussian noise component
+	fbeta <- (set_beta*exposure) + constant
+	data_point <- rnorm(1,fbeta,noise)
 	return (data_point)
 }
 
-gaussian_index_sample <- function(index){
+gaussian_index_sample <- function(x){
 	# returns a datapoint sampled from the index distribution
+	# This can be seen as an exposure to be used in the data_generator
 	# :param: index = cambridge index
-	index_mean <- index[1]
-	index_stdev <- index[2]
+	if (x == 1){
+		index_mean <- index_mean1
+		index_stdev <- index_stdev1
+	} else if (x == 2){
+		index_mean <- index_mean2
+		index_stdev <- index_stdev2
+	} else if (x == 3){
+		index_mean <- index_mean3
+		index_stdev <- index_stdev3
+	} else {
+		index_mean <- index_mean4
+		index_stdev <- index_stdev4
+	}
 	data_point <- rnorm(1, index_mean, index_stdev)
-
 	return (data_point)
 }
 
+index_s
+
+lambda_collector <- function(sample){
+	# returns the lambda calculated
+	return (lambda)
+}
+
+beta_collector <- function(sample){
+	# returns the beta calculated
+	return (beta)
+}
+
+calculate_index_from_paee <- function(paee){
+	# take paee value and calculate probability of it belonging to index
+	# random weighted assignment into that category
+	prob1 <- pnorm(paee, mean=index_mean1, sd=index_stdev1)
+	prob2 <- pnorm(paee, mean=index_mean2, sd=index_stdev2)
+	prob3 <- pnorm(paee, mean=index_mean3, sd=index_stdev3)
+	prob4 <- pnorm(paee, mean=index_mean4, sd=index_stdev4)
+
+	probs_cam_index <- c(prob1, prob2, prob3, prob4)
+	index_num <- sample(c(1:4), 1, replace=TRUE, prob=probs_cam_index )
+
+	return (index_num)
+}
+
+## Creation of datasets
+# Data set of 1600 points for which we calculate the lambda, (validation)
+validation_data <- as.data.frame(c(rep(1,400),rep(2,400),rep(3,400),rep(4,400)))
+colnames(validation_data) <- c("cam_index")
+validation_data$paee <- lapply(validation_data$cam_index, gaussian_index_sample) 
 
 
+# Data set of 20000 points for which we calculate the betas, (test)
+test_data <- as.data.frame(c(1:20000))
+colnames(test_data) <- c("id")
+test_data$paee <- runif(20000, min=paee_range_min, max=paee_range_max)
+test_data$cam_index <- lapply(test_data$paee, calculate_index_from_paee)
 
