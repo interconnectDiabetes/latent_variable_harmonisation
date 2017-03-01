1599823214e18bb35c7c6d48946088a3496ae78a
Paul Scherer <paul.scherer@mrc-epid.cam.ac.uk>
2017-03-01 13:33:57 +0000

    Moved creation of study and validation data for each cohort into functions

--- meta_analysis_bootstrap.R
+++ meta_analysis_bootstrap.R
@@ -27,54 +27,54 @@ stdDevs <- c(5,10,15)
 # Study Data Specific properties
 study_index_size <- 5000
 
+# Resampling trial number
+numtrials <- 1000
+
+
+###############################################################################
+########################### Functions #########################################
+###############################################################################
+
+validationCreate <- function(indices, stdDev, index_size){
+  # creates a validation set with given index size and std accompanied by the indices dataframe from each cohort
+  # Also comes with a paee
+  validation_study = data.frame(index = rep(x = indices$index_indicator, each= index_size))
+  validation_study$paee = unlist(unname(lapply(X = split(x = indices, f = as.factor(indices$index_indicator)), 
+    FUN = function(x){
+      output = rnorm(n = index_size, mean = x$mean, sd = stdDev)
+      return (output)
+  })))
+  return validation_study
+}
+
+studyCreate <- function(indices, stdDev, index_size){
+  # Creates a study dataset with indices presented, a standard deviation, and the size per index
+  # Also comes with a paee and the foo condition
+  study_data = data.frame(index = rep(x = indices$index_indicator,each= index_size))
+  study_data$paee = unlist(unname(lapply(X = split(x = indices, f = as.factor(indices$index_indicator)), 
+    FUN = function(x){
+        output = rnorm(n = index_size, mean = x$mean, sd = stdDev)
+        return (output)
+    })))
+  # generate our outcome variable plus some noise
+  study_data$foo <-   rnorm(length(study_data$paee),(set_beta*study_data$paee) + constant, 10)
+  return study_data
+}
+
 ###############################################################################
 ########################### COHORT CREATION ###################################
 ###############################################################################
 ## Cohort1
 indices1 <- data.frame(mean=c(30,40,50,60), std_dev = c(5,5,5,5), index_indicator = c(1,2,3,4))
-# might need to make a for loop to go through the creation of multiple validation sets
-validation_data1 <- data.frame(index = rep(x = indices$index_indicator, each = 25))
-validation_data1$paee = unlist(unname(lapply(X = split(x = indices, f = as.factor(indices$index_indicator)), 
-  FUN = function(x){
-    output = rnorm(n = validation_index_size[i], mean = x$mean, sd = x$std_dev)
-    return (output)
-  })))
-study_data1 = data.frame(index = rep(x = indices1$index_indicator,each= study_index_size))
-# for each index, pick all the values (defined by study_index_size) at once
-study_data1$paee = unlist(unname(lapply(X = split(x = indices1, f = as.factor(indices1$index_indicator)), 
-                                      FUN = function(x){
-                                          output = rnorm(n = study_index_size, mean = x$mean, sd = x$std_dev)
-                                          return (output)
-                                      })))
-# generate our outcome variable plus some noise
-study_data1$foo <-   rnorm(length(study_data$paee),(set_beta*study_data$paee) + constant,10)
-cohort1 <- list(indices = indices1, validation_index_size = c(25,50,100), validation_data = NULL, study_index_size = study_index_size, study_data = NULL)
 
 ## Cohort2
 indices2 <- data.frame(mean=c(25,35,45,55,65), std_dev = c(5,5,5,5,5), index_indicator = c(1,2,3,4,5))
-study_data2 = data.frame(index = rep(x = indices2$index_indicator,each= study_index_size))
-# for each index, pick all the values (defined by study_index_size) at once
-study_data2$paee = unlist(unname(lapply(X = split(x = indices2, f = as.factor(indices2$index_indicator)), 
-                                      FUN = function(x){
-                                          output = rnorm(n = study_index_size, mean = x$mean, sd = x$std_dev)
-                                          return (output)
-                                      })))
-# generate our outcome variable plus some noise
-study_data2$foo <-   rnorm(length(study_data$paee),(set_beta*study_data$paee) + constant,10)
-cohort2 <- list(indices = indices1, validation_index_size = c(25,50,100), validation_data = NULL, study_index_size = study_index_size, study_data = NULL)
 
 ## Cohort3
 indices3 <- data.frame(mean=c(30,35,40,45,50,55), std_dev = c(5,5,5,5,5,5), index_indicator = c(1,2,3,4,5,6))
-study_data3 = data.frame(index = rep(x = indices3$index_indicator,each= study_index_size))
-# for each index, pick all the values (defined by study_index_size) at once
-study_data3$paee = unlist(unname(lapply(X = split(x = indices3, f = as.factor(indices3$index_indicator)), 
-                                      FUN = function(x){
-                                          output = rnorm(n = study_index_size, mean = x$mean, sd = x$std_dev)
-                                          return (output)
-                                      })))
-# generate our outcome variable plus some noise
-study_data3$foo <-   rnorm(length(study_data$paee),(set_beta*study_data$paee) + constant,10)
-cohort3 <- list(indices = indices1, validation_index_size = c(25,50,100), validation_data = NULL, study_index_size = study_index_size, study_data = NULL)
 
+## Validation sets for each of the studies 
 
-
+cohort1 <- list(indices = indices1, validation_index_size = c(25,50,100), validation_data = NULL, study_index_size = study_index_size, study_data = study_data1)
+cohort2 <- list(indices = indices1, validation_index_size = c(25,50,100), validation_data = NULL, study_index_size = study_index_size, study_data = study_data2)
+cohort3 <- list(indices = indices1, validation_index_size = c(25,50,100), validation_data = NULL, study_index_size = study_index_size, study_data = study_data3)
