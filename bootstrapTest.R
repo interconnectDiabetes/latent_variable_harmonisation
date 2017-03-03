082eb0698e3fef6d2d62a72ee4b058e29a17df0a
Paul Scherer <paul.scherer@mrc-epid.cam.ac.uk>
2017-03-03 17:28:02 +0000

    bootstrap regressions and results saving

--- bootstrapTest.R
+++ bootstrapTest.R
@@ -27,7 +27,8 @@ validation_index_sizes <- c(25,50,100)
 validation_index_size <- 25 # temp
 
 # Bootstrapping properties
-num_trials <- 1000
+bootstrap_trials <- 100
+boostrap_index_size <- 10
 
 ###############################################################################
 ########################### Functions #########################################
@@ -58,9 +59,10 @@ createValidationData <- function(coh_base, validation_index_size) {
 ###############################################################################
 ########################### COHORT CREATION ###################################
 ###############################################################################
+# overall results dataframe
 results_df <- data.frame()
-betas <- vector("numeric")
-std_errs <- vector("numeric")
+
+
 
 # Defining a base generator for one cohort (which spawns validation, study data)
 coh_base = data.frame(means = c(30,40,50,60), indices = c(1,2,3,4), std_dev = 5) # change stddev and index size to variables later
@@ -71,19 +73,36 @@ study_data = createStudyData(coh_base, study_index_size)
 # spawn the validation data
 validation_data = createValidationData(coh_base, validation_index_size)
 
-# spawn the bootstrap of the validation
-# Resampling step (ie create the bootstrap)
-boostrap_index_size <- 10
-bootstrap_validation <- data.frame(index = rep(x = coh_base$indices, each= boostrap_index_size))
-bootstrap_validation$paee <- unlist(unname(lapply(X = split(x=validation_data$paee, f= as.factor(validation_data$index)), 
-	FUN = sample, size = boostrap_index_size,replace=TRUE)))
+# per cohort base result vector
+betas <- vector("numeric")
+std_errs <- vector("numeric")
 
-# Regression and storing of regression coefficients and stderrs
-study_data$paee_sample_ind_mean <- unlist(unname(lapply(X = split(x=bootstrap_validation$paee, f= as.factor(bootstrap_validation$index)),
-  FUN = function(paee_vals){
-    output = rep(x = mean(paee_vals), times = study_index_size)
-    return(output)
-  })))
-reg_out_ind_mean <- lm(formula=foo~paee_sample_ind_mean, data=study_data)
-reg_coeff_ind_mean <- reg_out_ind_mean$coefficients["paee_sample_ind_mean"]
-reg_std_ind_mean <- (summary(reg_out_ind_mean)$coefficients[,"Std. Error"])["paee_sample_ind_mean"]
+# bootstrapping trials times
+# spawn the bootstrap of the validation
+# Resampling step (ie create the bootstrap)#
+for (i in 1:bootstrap_trials){
+	bootstrap_validation <- data.frame(index = rep(x = coh_base$indices, each= boostrap_index_size))
+	bootstrap_validation$paee <- unlist(unname(lapply(X = split(x=validation_data$paee, f= as.factor(validation_data$index)), 
+		FUN = sample, size = boostrap_index_size,replace=TRUE)))
+
+	# Regression and storing of regression coefficients and stderrs
+	study_data$paee_sample_ind_mean <- unlist(unname(lapply(X = split(x=bootstrap_validation$paee, f= as.factor(bootstrap_validation$index)),
+	  FUN = function(paee_vals){
+	    output = rep(x = mean(paee_vals), times = study_index_size)
+	    return(output)
+	  })))
+	reg_out_ind_mean <- lm(formula=foo~paee_sample_ind_mean, data=study_data)
+	reg_coeff_ind_mean <- reg_out_ind_mean$coefficients["paee_sample_ind_mean"]
+	reg_std_ind_mean <- (summary(reg_out_ind_mean)$coefficients[,"Std. Error"])["paee_sample_ind_mean"]
+
+	betas <- c(betas, reg_coeff_ind_mean)
+	std_errs <- c(std_errs, reg_std_ind_mean)
+}
+
+results <- as.data.frame(c(1:bootstrap_trials))
+colnames(results) <- c("BootStrap Trial")
+results$valid_size <- rep(x=validation_index_size, times = bootstrap_trials)
+results$reg_coeff_per_mean <- betas
+results$reg_stdError_per_mean <- std_errs
+results_df <- rbind(results_df, results)
+
