25ae07c8dfeb84339993a2381a3618a0692ac336
Paul Scherer <paul.scherer@mrc-epid.cam.ac.uk>
2017-03-15 15:19:37 +0000

    parrallelized bootstrap process for speed up and removal of unused variables

--- testerMulti.R
+++ testerMulti.R
@@ -8,16 +8,16 @@
 setwd('V:/Studies/InterConnect/Internal/Latent variable harmonisation/plots')
 library(ggplot2)
 library(reshape2)
-# library(parallel)
+library(parallel)
 
 
 ###############################################################################
 ########################### DATA AND SETTINGS #################################
 ###############################################################################
-# # Calculate the number of cores
-# no_cores <- detectCores() - 2
-# # Initiate cluster
-# cl <- makeCluster(no_cores)
+# Calculate the number of cores
+no_cores <- detectCores() - 1
+# Initiate cluster
+cl <- makeCluster(no_cores)
 
 # Base Data set properties
 set_beta <- 0.5
@@ -26,15 +26,8 @@ constant <- 20
 # Study Data properties
 study_index_size <- 5000
 
-# Validation Data properties
-validation_index_size <- 25 
-
-# Bootstrapping properties
-bootstrap_trials <- 100
-boostrap_index_size <- 10
-
 # Test Properties
-num_trials <- 5
+num_trials <- 1000
 ###############################################################################
 ########################### Functions #########################################
 ###############################################################################
@@ -73,33 +66,14 @@ bootstrapRun <- function(coh_base, study_index_size, validation_index_size) {
 	# overall results dataframe
 	results_df <- data.frame()
 
-	# for (i in 1:num_trials){
-	# 	bootstrap_validation <- data.frame(index = rep(x = coh_base$indices, each= boostrap_index_size))
-	# 	bootstrap_validation$paee <- unlist(unname(lapply(X = split(x=validation_data$paee, f= as.factor(validation_data$index)), 
-	# 		FUN = sample, size = boostrap_index_size,replace=TRUE)))
-
-	# 	# Regression and storing of regression coefficients and stderrs
-	# 	study_data$paee_sample_ind_mean <- unlist(unname(lapply(X = split(x=bootstrap_validation$paee, f= as.factor(bootstrap_validation$index)),
-	# 	  FUN = function(paee_vals){
-	# 	    output = rep(x = mean(paee_vals), times = study_index_size)
-	# 	    return(output)
-	# 	  })))
-	# 	reg_out_ind_mean <- lm(formula=foo~paee_sample_ind_mean, data=study_data)
-	# 	reg_coeff_ind_mean <- reg_out_ind_mean$coefficients["paee_sample_ind_mean"]
-	# 	reg_std_ind_mean <- (summary(reg_out_ind_mean)$coefficients[,"Std. Error"])["paee_sample_ind_mean"]
-
-	# 	betas <- c(betas, reg_coeff_ind_mean)
-	# 	std_errs <- c(std_errs, reg_std_ind_mean)
-	# }
-
 	results <- as.data.frame(c(1:num_trials))
 	colnames(results) <- c("NumTrial")
 	results$valid_size <- rep(x=validation_index_size, times = num_trials)
 	results$standard_deviation <- rep(x=coh_base$std_dev[1], times = num_trials)
-	results$reg_coeff_per_mean  <- lapply(X=1:num_trials, FUN=function(x){
-		bootstrap_validation <- data.frame(index = rep(x = coh_base$indices, each= boostrap_index_size))
+	results$reg_coeff_per_mean  <- parLapply(cl, X=1:num_trials, fun=function(x){
+		bootstrap_validation <- data.frame(index = rep(x = coh_base$indices, each= validation_index_size))
 		bootstrap_validation$paee <- unlist(unname(lapply(X = split(x=validation_data$paee, f= as.factor(validation_data$index)), 
-			FUN = sample, size = boostrap_index_size,replace=TRUE)))
+			FUN = sample, size = validation_index_size,replace=TRUE)))
 
 		# Regression and storing of regression coefficients and stderrs
 		study_data$paee_sample_ind_mean <- unlist(unname(lapply(X = split(x=bootstrap_validation$paee, f= as.factor(bootstrap_validation$index)),
@@ -142,24 +116,20 @@ absDiff <- function(x,y){
 numSeeds <- 5
 minmax_list = vector(mode="list", length = numSeeds) 
 
+#progress bar for seeds completed
 pbt <- txtProgressBar(min = 1, max = numSeeds, style = 3)
 for (seeds in 1:numSeeds){
   set.seed(seeds)
-	# Get a large dataframe of results
 	results = data.frame()
-	# progressbar
-	# pb <- txtProgressBar(min = 25, max = 100, style = 3)
-	for (i in seq(from=25, to=100, by=5)){
+	for (validation_index_size in seq(from=25, to=100, by=5)){
 		for (standard_dev in 5:15){
 			# Defining a base generator for one cohort (which spawns validation, study data)
 			coh_base = data.frame(means = c(30,40,50,60), indices = c(1,2,3,4), std_dev = standard_dev) # change stddev and index size to variables later
-			bootRunResult = bootstrapRun(coh_base, study_index_size, i)
+			bootRunResult = bootstrapRun(coh_base, study_index_size, validation_index_size)
 			bootRunResult = cbind(bootRunResult, standard_dev)
 			results = rbind(results, bootRunResult)
 		}
-		# setTxtProgressBar(pb,i)
 	}
-	# close(pb)
 	# Create a heatmap of 'accuracy' through absolute difference in the 2.5% and 97.5% tiles
 	results$minMaxDiff <- unlist(unname(mapply(FUN=absDiff, results$`x_2.5%`, results$`x_97.5%`)))
 
@@ -169,7 +139,7 @@ for (seeds in 1:numSeeds){
 close(pbt)
 
 # #stop our cluster
-# stopCluster(cl)
+stopCluster(cl)
 
 # Then put the sum of the minmaxes together into the results dataframe
 results$minMaxDiff <- Reduce("+", x = minmax_list)
